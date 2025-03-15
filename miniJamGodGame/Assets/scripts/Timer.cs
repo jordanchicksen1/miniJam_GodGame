e1ada6cbd46a0573983a545cf835daf20cebc3ef
jordanchicksen1 <2333640@students.wits.ac.za>
2025-03-15 15:31:17 +0200

    did a lot of stuff

--- miniJamGodGame/Assets/scripts/Timer.cs
+++ miniJamGodGame/Assets/scripts/Timer.cs
@@ -1,12 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float Timez;
-    void Update()
+    public float currentTime;
+    public float maxTime = 5f;
+    public Image TimerBar;
+
+    public GameObject nextQuestion;
+    public GameObject currentQuestion;
+
+    public void updateTime(float amount)
     {
-        Timez += Time.deltaTime;
+        currentTime += amount;
+        updateTimeBar();
+        Debug.Log("should decrease");
+    }
+
+    public void Update()
+    {
+        currentTime -= Time.deltaTime;
+        updateTimeBar();
+        nextQ();
+    }
+
+    public void updateTimeBar()
+    {
+        float targetFillAmount = currentTime / maxTime;
+        TimerBar.fillAmount = targetFillAmount;
+    }
+
+    public void nextQ() 
+    {
+        if (currentTime <= 0 ) 
+        {
+            Debug.Log("0");
+            currentQuestion.SetActive(false);
+            nextQuestion.SetActive(true);
+        }
     }
 }
